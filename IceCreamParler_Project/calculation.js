82c701bf0993c428de53661036a4831b85aaea37
sourav <souravpaul01032002@gmail.com>
2022-11-22 02:41:33 +0530

    First commit

---
+++ IceCreamParler_Project/calculation.js
@@ -0,0 +1,192 @@
+// var Quantity = parseInt( document.getElementById("quantity"));
+function js() {
+
+                        // VANILLA
+    //vanilla + cup
+    if (document.getElementById('flavour').value === 'vanilla' && document.getElementById('hold').value === 'cup') {
+        var amount = 30 + 10;
+        //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+
+    //vanilla + Glass
+    if (document.getElementById('flavour').value === 'vanilla' && document.getElementById('hold').value === 'Glass') {
+        var amount = 30 + 20;
+         //Checking the Quantity condition  
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+       
+    }
+
+    //vanilla + pot
+    if (document.getElementById('flavour').value === 'vanilla' && document.getElementById('hold').value === 'pot') {
+        var amount = 30 + 25;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+       
+    }
+
+
+                        //CHOCOLATE
+    //Chocolate + cup
+    if (document.getElementById('flavour').value === 'Chocolate' && document.getElementById('hold').value === 'cup') {
+        var amount = 40 + 10;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+
+    //Chocolate + Glass
+    if (document.getElementById('flavour').value === 'Chocolate' && document.getElementById('hold').value === 'Glass') {
+        var amount = 40 + 20;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+
+    //Chocolate + pot
+    if (document.getElementById('flavour').value === 'Chocolate' && document.getElementById('hold').value === 'pot') {
+        var amount = 40 + 25;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+    // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.com%2Fice-cream-cone-with-colorful-sprinkles-on-yellow-background-image183536066.html&psig=AOvVaw1-sUyr44yP59BX4uLnb_1_&ust=1669111506184000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjaxoSDv_sCFQAAAAAdAAAAABAE
+
+    
+                        //STRAWBERRY
+    //strawberry + cup
+    if (document.getElementById('flavour').value === 'strawberry' && document.getElementById('hold').value === 'cup') {
+        var amount = 20 + 10;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+
+    //strawberry + Glass
+    if (document.getElementById('flavour').value === 'strawberry' && document.getElementById('hold').value === 'Glass') {
+        var amount = 20 + 20;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+
+    //strawberry + pot
+    if (document.getElementById('flavour').value === 'strawberry' && document.getElementById('hold').value === 'pot') {
+        var amount = 20 + 25;
+         //Checking the Quantity condition
+        if (document.getElementById('quantity').value==='1') {
+            var amount = amount*1
+        }
+        else if (document.getElementById('quantity').value==='2') {
+            var amount = amount*2
+        }
+        else if (document.getElementById('quantity').value==='3') {
+            var amount = amount*3
+        }
+        else if (document.getElementById('quantity').value==='4') {
+            var amount = amount*4
+        }
+        document.write("Your order is coming soon...")
+        document.write("Your Total Amount is :"+amount);
+    }
+ 
+}
